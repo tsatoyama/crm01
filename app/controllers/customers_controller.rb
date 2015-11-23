4cacd9d15ff938f8a329af12919745aaa39bacd1
Satoyama Tomoaki <tsatoyama@vitalify.jp>
2015-11-23 13:59:08 +0900

    editとupdateの追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -32,9 +32,16 @@ class CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params) == true then
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
