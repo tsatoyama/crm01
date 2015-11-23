e225248d18c537a3cba9f0276acb98e8f1942118
Satoyama Tomoaki <tsatoyama@vitalify.jp>
2015-11-23 10:37:03 +0900

    createメソッド作成＆ストロングパラメータの追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,6 +7,22 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to @customer
+
+    # redirect_to @customerの場合、以下と同じ挙動になる模様
+    # redirect_to "/customers/#{@customer.id}"
+    # redirect_to customer_url(id: @customer.id)
+    # redirect_to customer_url(id: @customer.to_param)
+  end
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+      )
   end
 
   def edit
