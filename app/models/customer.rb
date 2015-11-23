2c0bf10e4ef70da71ab9ef404ec31d2cc8016f60
Satoyama Tomoaki <tsatoyama@vitalify.jp>
2015-11-23 12:14:01 +0900

    バリデーション定義の追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,16 @@ class Customer < ActiveRecord::Base
     full_name
   end
 
+  # バリデーションの定義
+  # saveメソッドが呼ばれたタイミングで、モデルのバリデーションが実行される
+  # バリデーションが通って保存されたらtrue
+  # 通らなかったら保存しないでfalse
+  validates :family_name,
+    presence: true, length: { maximum: 20 }
+  validates :given_name,
+    presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+    presence: true, format: { with: VALID_EMAIL_REGEX }
+
 end
