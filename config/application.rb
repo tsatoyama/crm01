e7371bde482aa72e2d8f0a56beb6b49c97f2b27d
Satoyama Tomoaki <tsatoyama@vitalify.jp>
2015-11-23 09:58:13 +0900

    タイムゾーン、デフォルトロケールの編集

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Crm01
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Add Start
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    # Add End
   end
 end
