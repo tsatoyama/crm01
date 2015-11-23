a2fc32e4fc799bf16b2e114eed919fe22403a28a
Satoyama Tomoaki <tsatoyama@vitalify.jp>
2015-11-23 14:30:36 +0900

    seedスクリプトにFakerを適用

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 100.times do |index|
+#   Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
+# end
+
 100.times do |index|
-  Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
+  Customer.create(
+    family_name: Faker::Japanese::Name.last_name,
+    given_name: Faker::Japanese::Name.first_name,
+    email: "customer_#{index}@sparta.com"
+  )
 end
